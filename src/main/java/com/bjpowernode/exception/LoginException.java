8506529aa2222168266ab3227c1b67227edb86d9
Dee <rreezii@outlook.com>
2023-03-18 16:31:21 +0800

    crm-v1.3.x

---
+++ src/main/java/com/bjpowernode/exception/LoginException.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.exception;
+
+/**
+ * 
+ *
+ * @author Dee
+ * @date 2023-03-17 10:24
+ */
+public class LoginException {
+}
---
+++ src/main/java/com/bjpowernode/filter/LoginFilter.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.filter;
+
+/**
+ * 
+ *
+ * @author Dee
+ * @date 2023-03-17 15:44
+ */
+public class LoginFilter {
+}
